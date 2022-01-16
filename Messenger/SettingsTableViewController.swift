76d5c3c0a76a055f93b27977df429e333f700cfb
Sergey Chukanov <schukanov@gmail.com>
2022-01-16 16:20:51 +0300

    Settings tab layout and all the stuff

---
+++ Messenger/SettingsTableViewController.swift
@@ -0,0 +1,73 @@
+//
+//  SettingsTableViewController.swift
+//  Messenger
+//
+//  Created by Afir Thes on 16.01.2022.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+
+    @IBOutlet weak var appVersionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        if #available(iOS 15.0, *) {
+            tableView.sectionHeaderTopPadding = 0
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+    
+        showUserInfo()
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return section == 0 ? 0.0 : 10.0
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor(named: "tableViewBackgroundColor")
+        return headerView
+    }
+
+    @IBAction func tellAFriendButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func termsAndConditionsButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func logOutButtonPressed(_ sender: Any) {
+        
+    }
+    
+    private func showUserInfo() {
+        if let user = User.currentUser {
+            userNameLabel.text = user.userName
+            statusLabel.text = user.status
+            appVersionLabel.text = "App version \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")"
+            if user.avatarLink != "" {
+                // download and set avatar image
+                
+            }
+        }
+    }
+    
+}
